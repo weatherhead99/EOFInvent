64ba5d1b5e75a688ef95e4d66dbd8f822d732e3c
Dan Weatherill <daniel.weatherill@physics.ox.ac.uk>
2018-04-16 14:51:58 +0100

    more schema stuff

--- EOFinvent/db/schema.py
+++ EOFinvent/db/schema.py
@@ -7,7 +7,9 @@ Created on Mon Apr 16 14:21:15 2018
 """
 
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, Integer, String, Enum
+from sqlalchemy import Column, Integer, String, Enum, Boolean, Sequence, Date
+from sqlalchemy import ForeignKey
+from sqlalchemy.orm import relationship
 
 from sqlalchemy import create_engine
 import enum
@@ -21,19 +23,37 @@ class Provenance(enum.Enum):
     LongTermLoan = 3
     ShortTermLoan = 4
 
-
 class User(Base):
     __tablename__ = "users"
-    id = Column(Integer, primary_key=True)
+    id = Column(Integer, Sequence("userid_seq"), primary_key=True)
     name = Column(String)
     email = Column(String)
+
+class InventoryCategories(Base):
+    __tablename__ = "categories"
+    id = Column(Integer, primary_key=True)
+    name = Column(String)
+
+class InventoryLocations(Base):
+    __tablename__ = "locations"
+    id = Column(Integer, primary_key=True)
+
 
 class InventoryItem(Base):
     __tablename__ = "items"
     id = Column(Integer, primary_key=True)
     provenance = Column(Enum(Provenance))
+    donothack = Column(Boolean)
+    name = Column(String)
+    acquired = Column(Date)
+    user_responsible = Column(Integer, ForeignKey("users.id"))
     
-
+    category = Column(Integer, ForeignKey("categories.id"))
+    location = Column(Integer, ForeignKey("locations.id"))
+    
+    user_rel = relationship("User", back_populates="items")
+    category_rel = relationship("InventoryCategories", back_populates="items")
+    location_rel = relationship("InventoryLocations", back_populates="items")
 
 
 
